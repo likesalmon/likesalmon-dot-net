f161f337062a064b19f35a023b6adb52b0174a54
Ammon Morris <likesalmon@gmail.com>
2025-03-17 16:41:54 -0800

    feat(hero): animations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,34 @@
+"use client";
 import Image from "next/image";
 import Section from "./Section";
+import { motion } from "motion/react";
 
 export default function Hero() {
   return (
     <Section className="items-center justify-center gap-12 relative" id="hero">
-      <Image src="/salmon-icon-380x338.png" alt="Likesalmon" width={380} height={338} />
-      <div className="font-exo text-4xl font-bold text-center leading-relaxed text-white">
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.8 }}>
+        <Image src="/salmon-icon-380x338.png" alt="Likesalmon" width={380} height={338} />
+      </motion.div>
+      <motion.div
+        className="font-exo text-4xl font-bold text-center leading-relaxed text-white"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.4 }}
+      >
         Developer & UI Designer{" "}
         <span className="bg-gradient-to-r from-teal-500 to-fuchsia-500 bg-clip-text text-transparent inline-block">
           Ammon* Morris
         </span>
         <br /> bridges the divide between technology and design.
-      </div>
-      <div className="font-exo text-4xl font-bold text-center font-light text-lg text-fuchsia-400">
+      </motion.div>
+      <motion.div
+        className="font-exo text-4xl font-bold text-center font-light text-lg text-fuchsia-400"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.6 }}
+      >
         * Pronounced /ˈæmən/ - like &quot;salmon&quot;
-      </div>
+      </motion.div>
     </Section>
   );
 }
